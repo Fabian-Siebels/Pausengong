cf37da629f062c482e4ca67c95fc3c61a6b9a54b
Fabian Siebels <40269039+Fabian-Siebels@users.noreply.github.com>
2018-09-14 09:21:55 +0200

    Update gong.py

--- gong.py
+++ gong.py
@@ -6,25 +6,28 @@ from time import sleep
 
 
 subprocess.Popen(["aplay","/home/pi/gong.wav"], stdin=subprocess.PIPE)
-sleep()
+sleep(10)
 
 sys.exit()
 
 #Cronjobs
 
-# Montag bis Donnerstag
-
-# 7:15 Uhr:     0 15 7 ? * MON,TUE,WED,THU * /usr/bin/python /home/pi/gong.py >/dev/null 2>&1      
-# 9:30 Uhr:     0 30 9 ? * MON,TUE,WED,THU * /usr/bin/python /home/pi/gong.py >/dev/null 2>&1
-
-# 11:30 Uhr:    0 30 11 ? * MON,TUE,WED,THU * /usr/bin/python /home/pi/gong.py >/dev/null 2>&1   
-# 14:45 Uhr:    0 45 14 ? * MON,TUE,WED,THU * /usr/bin/python /home/pi/gong.py >/dev/null 2>&1
-
-# 16:15 Uhr:    0 15 16 ? * MON,TUE,WED,THU * /usr/bin/python /home/pi/gong.py >/dev/null 2>&1
-
-# Freitag
-
-# 7:00 Uhr:     0 0 7 ? * FRI * /usr/bin/python /home/pi/gong.py >/dev/null 2>&1
-# 9:30 Uhr:     0 30 9 ? * FRI * /usr/bin/python /home/pi/gong.py >/dev/null 2>&1
-
-# 12:00 Uhr:    0 0 12 ? * FRI * /usr/bin/python /home/pi/gong.py >/dev/null 2>&1
+#Anfang MON-DON
+#*/15 7 * * 1-4 /usr/bin/python /home/pi/gong.py >/dev/null 2>&1
+#Fruehstueck
+#*/30 9 * * 1-4 /usr/bin/python /home/pi/gong.py >/dev/null 2>&1
+#*/45 9 * * 1-4 /usr/bin/python /home/pi/gong.py >/dev/null 2>&1
+#Mittag
+#*/30 11 * * 1-4 /usr/bin/python /home/pi/gong.py >/dev/null 2>&1
+#*/ 12 * * 1-4 /usr/bin/python /home/pi/gong.py >/dev/null 2>&1
+#Nachmittag
+#*/45 14 * * 1-4 /usr/bin/python /home/pi/gong.py >/dev/null 2>&1
+#*/ 15 * * 1-4 /usr/bin/python /home/pi/gong.py >/dev/null 2>&1
+#Freitag
+#Anfang
+#*/ 7 * * 1-4 /usr/bin/python /home/pi/gong.py >/dev/null 2>&1
+#Fruehstueck
+#*/30 9 * * 5 /usr/bin/python /home/pi/gong.py >/dev/null 2>&1
+#*/45 9 * * 1-4 /usr/bin/python /home/pi/gong.py >/dev/null 2>&1
+#Frei
+#*/ 12 * * 1-4 /usr/bin/python /home/pi/gong.py >/dev/null 2>&1
