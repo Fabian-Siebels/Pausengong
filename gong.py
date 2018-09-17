01c93f73b037c475274080cd2b288d596cc4c22d
Fabian Siebels <40269039+Fabian-Siebels@users.noreply.github.com>
2018-09-17 14:12:18 +0200

    Update gong.py

--- gong.py
+++ gong.py
@@ -6,8 +6,8 @@ import sys
 from time import sleep
 
 #Öffnen und abspielen einer .wav Datei
-# /home/pi/... ist der Pfad, wo sich die .wav Datei befindet.
-subprocess.Popen(["aplay","/home/pi/gong.wav"], stdin=subprocess.PIPE)
+# /home/pi/Pausengong/... ist der Pfad, wo sich die .wav Datei befindet.
+subprocess.Popen(["aplay","/home/pi/Pausengong/gongsound.wav"], stdin=subprocess.PIPE)
 sleep(10)
 
 sys.exit()
@@ -16,22 +16,19 @@ sys.exit()
 
 #Cronjobs
 
-#Anfang MON-DON
-#*/15 7 * * 1-4 /usr/bin/python /home/pi/gong.py >/dev/null 2>&1
-#Fruehstueck
-#*/30 9 * * 1-4 /usr/bin/python /home/pi/gong.py >/dev/null 2>&1
-#*/45 9 * * 1-4 /usr/bin/python /home/pi/gong.py >/dev/null 2>&1
-#Mittag
-#*/30 11 * * 1-4 /usr/bin/python /home/pi/gong.py >/dev/null 2>&1
-#*/ 12 * * 1-4 /usr/bin/python /home/pi/gong.py >/dev/null 2>&1
-#Nachmittag
-#*/45 14 * * 1-4 /usr/bin/python /home/pi/gong.py >/dev/null 2>&1
-#*/ 15 * * 1-4 /usr/bin/python /home/pi/gong.py >/dev/null 2>&1
-#Freitag
-#Anfang
-#*/ 7 * * 5 /usr/bin/python /home/pi/gong.py >/dev/null 2>&1
-#Fruehstueck
-#*/30 9 * * 5 /usr/bin/python /home/pi/gong.py >/dev/null 2>&1
-#*/45 9 * * 5 /usr/bin/python /home/pi/gong.py >/dev/null 2>&1
-#Frei
-#*/ 12 * * 5 /usr/bin/python /home/pi/gong.py >/dev/null 2>&1
+#| Uhrzeit | Cronjob |
+#|--|--|
+#|Mon-Don||
+#| 7:15 | `*/15 7 * * 1-4 /usr/bin/python /home/pi/Pausengong/gong.py >/dev/null 2>&1` |
+#| 9:30 | `*/30 9 * * 1-4 /usr/bin/python /home/pi/Pausengong/gong.py >/dev/null 2>&1` |
+#| 9:45 | `*/45 9 * * 1-4 /usr/bin/python /home/pi/Pausengong/gong.py >/dev/null 2>&1` |
+#|11:30 | `*/30 11 * * 1-4 /usr/bin/python /home/pi/Pausengong/gong.py >/dev/null 2>&1`|
+#|12:00 |`*/ 12 * * 1-4 /usr/bin/python /home/pi/Pausengong/gong.py >/dev/null 2>&1`|
+#|14:45 |`*/45 14 * * 1-4 /usr/bin/python /home/pi/Pausengong/gong.py >/dev/null 2>&1`|
+#|15:00 |`*/ 15 * * 1-4 /usr/bin/python /home/pi/Pausengong/gong.py >/dev/null 2>&1`|
+#|16:15 |`*/15 16 * * 1-4 /usr/bin/python /home/pi/Pausengong/gong.py >/dev/null 2>&1`|
+#|Fri||
+#|7:00 |`*/ 7 * * 5 /usr/bin/python /home/pi/Pausengong/gong.py >/dev/null 2>&1`|
+#|9:30 |`*/30 9 * * 5 /usr/bin/python /home/pi/Pausengong/gong.py >/dev/null 2>&1`|
+#|9:45 |`*/45 9 * * 5 /usr/bin/python /home/pi/Pausengong/gong.py >/dev/null 2>&1`| 
+#|12:00 |`*/ 12 * * 5 /usr/bin/python /home/pi/Pausengong/gong.py >/dev/null 2>&1`|   
